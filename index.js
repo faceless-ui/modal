5da014754e136b35a9ca91fb95c97e520590df9d
Jacob Fletcher <jacobsfletch@gmail.com>
2019-10-02 15:47:41 -0400

    adds asModal and ModalContext to named exports

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+export { default as asModal } from './asModal';
 export { default as ModalContainer } from './ModalContainer';
+export { default as ModalContext } from './ModalProvider/context';
 export { default as ModalProvider } from './ModalProvider';
 export { default as ModalToggler } from './ModalToggler';
