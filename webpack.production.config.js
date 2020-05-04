05b226708ebfdcb839bf360c18af40743c22a3c9
Jacob Fletcher <jacobsfletch@gmail.com>
2020-05-04 15:09:33 -0400

    deprecates Netlify build script and Webpack config, intergrates into production

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+module.exports = [{
   devtool: 'source-map',
   mode: 'production',
   entry: './src/index.js',
@@ -27,4 +28,26 @@ module.exports = {
     'react-dom': 'react-dom',
     'react-transition-group': 'react-transition-group',
   },
-};
+}, {
+  devtool: 'source-map',
+  mode: 'production',
+  entry: './demo/index.js',
+  output: {
+    filename: 'demo.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: 'demo/index.html',
+    }),
+  ],
+}];
