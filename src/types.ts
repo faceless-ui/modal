e38a083e6b8fdc7edf24404753a7da928249390a
Jacob Fletcher <jacobsfletch@gmail.com>
2024-06-07 19:38:32 -0400

    includes demo in tsconfig but excludes from bundle

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
-export { ModalProviderProps } from './ModalProvider/index.js';
-export { IModalContext } from './ModalProvider/context.js';
-export { ModalContainerProps } from './ModalContainer/index.js';
-export { ModalTogglerProps } from './ModalToggler/index.js';
-export { ModalProps } from './Modal/index.js';
+export type { ModalProviderProps } from './ModalProvider/index.js';
+export type { IModalContext } from './ModalProvider/context.js';
+export type { ModalContainerProps } from './ModalContainer/index.js';
+export type { ModalTogglerProps } from './ModalToggler/index.js';
+export type { ModalProps } from './Modal/index.js';
