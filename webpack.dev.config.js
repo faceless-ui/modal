672596e8e8bf23c93491e68e4adbf2d8f91aa192
Jacob Fletcher <jacobsfletch@gmail.com>
2024-06-08 12:34:03 -0400

    sets module resolution to node in dev mode

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -22,6 +22,10 @@ export default {
         loader: 'ts-loader',
         options: {
           transpileOnly: true,
+          compilerOptions: {
+            module: "ESNext",
+            moduleResolution: "node",
+          }
         },
       }
     ],
